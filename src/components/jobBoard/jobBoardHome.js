587c7115e76a5e5a14154eb6bd221d406cab0ddf
Brodie Brock <brodiebrock@gmail.com>
2019-08-02 11:30:13 -0400

    messing with styles

--- src/components/jobBoard/jobBoardHome.js
+++ src/components/jobBoard/jobBoardHome.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AddJobBoard from './addJobBoard';
 
-
 class JobBoardHome extends Component {
     state = {
         jobs: []
