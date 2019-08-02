6a7306f3b8b07353ab84340d3b5f27c4dcb86947
Brodie Brock <brodiebrock@gmail.com>
2019-08-01 23:31:34 -0400

    messing with styles

--- src/components/home.js
+++ src/components/home.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Weather from './weather/weather';
 import JobBoardHP from './jobBoard/jobBoardHP'
-import { Table } from "bloomer";
+import '../App.css'
 
 class HomePage extends Component {
   render() {
     return (
             <>
+            <div className='home-photo'></div>
                 <Weather />
                 <JobBoardHP />
             </>
